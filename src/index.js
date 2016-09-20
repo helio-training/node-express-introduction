537b4039ea02d04272f27a5e0f749d785be43998
Tyler Garlick <tjgarlick@gmail.com>
2016-09-19 18:51:06 -0600

    Url parameters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const Express = require('express');
 const app = Express();
 
+//
+// app.engine('html', require('ejs').renderFile);
+
 app.use(Express.static('public'));
 
 // app.get('/', (request, response) => {
 //   return response.send('Hello');
 // });
 
-app.get('/products', (request, response) => {
-  return response.send('My awesome products');
+// app.get('/products', (request, response) => {
+//   return response.send('My awesome products');
+// });
+
+
+app.get('/sports/:sport/qb/top-rated', (request, response) => {
+  const sport = request.params.sport;
+  console.log(request.params);
+  return response.send(`${sport}: Top Rated QB's`);
 });
 
 
